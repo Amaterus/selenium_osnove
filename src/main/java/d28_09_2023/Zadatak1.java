db1a76a76b168a46b56ea5a864e9245b15453dbc
Veljko Andjelkovic <veljkoandjelkovic2000@gmail.com>
2023-09-30 15:39:58 +0200

    Add Automation of GitHub Repository Filtering.

---
+++ src/main/java/d28_09_2023/Zadatak1.java
@@ -0,0 +1,30 @@
+package d28_09_2023;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Zadatak1 {
+    public static void main(String[] args)  {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+
+        driver.get("https://github.com/orgs/embedly/repositories?q=&type=all&language=&sort=");
+
+        driver.findElement(By.xpath("//*[@id='type-options']")).click();
+        driver.findElement(By.xpath("//div[@class='SelectMenu-list']/label[2]")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#org-repositories a.issues-reset-query"))).click();
+
+        driver.quit();
+
+    }
+}
