0c24ad4d9900f39d2e274e3f078a31a787b9df08
Veljko Andjelkovic <veljkoandjelkovic2000@gmail.com>
2023-10-12 22:33:45 +0200

    Verify the URL for the cart page

--- src/test/java/tests/BasicTest.java
+++ src/test/java/tests/BasicTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -8,10 +9,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import pages.InventoryPage;
-import pages.LeftNavPage;
-import pages.LoginPage;
-import pages.TopNavPage;
+import pages.*;
 
 import java.time.Duration;
 
@@ -23,8 +21,10 @@ public abstract class BasicTest {
     protected LeftNavPage leftNavPage;
     protected InventoryPage inventoryPage;
     protected TopNavPage topNavPage;
+    protected CartPage cartPage;
+
     @BeforeClass
-    public void setup () {
+    public void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -35,18 +35,23 @@ public abstract class BasicTest {
         leftNavPage = new LeftNavPage(driver, wait);
         inventoryPage = new InventoryPage(driver, wait);
         topNavPage = new TopNavPage(driver, wait);
+        cartPage = new CartPage(driver, wait);
 
     }
+
     @BeforeMethod
-    public void beforeMethod () {
+    public void beforeMethod() {
         driver.navigate().to(baseUrl);
     }
+
     @AfterMethod
-    public void afterMethod () {
+    public void afterMethod() {
         driver.manage().deleteAllCookies();
+        ((JavascriptExecutor)driver).executeScript("window.localStorage.clear();");
     }
+
     @AfterClass
-    public void afterClass () {
+    public void afterClass() {
         driver.quit();
     }
 }
