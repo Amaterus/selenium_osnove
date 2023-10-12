bdcbb4ccad43c9818a353ead8e1b4c9a777dbc53
Veljko Andjelkovic <veljkoandjelkovic2000@gmail.com>
2023-10-12 23:31:50 +0200

    Verify if logout option is working on the Cart Page

--- src/main/java/pages/LeftNavPage.java
+++ src/main/java/pages/LeftNavPage.java
@@ -62,4 +62,10 @@ public class LeftNavPage extends BasicPage{
     public void clickOnTheAboutFromMenuOption () {
         getTheAboutFromMenuOption().click();
     }
+    public WebElement getTheLogoutFromMenuOption () {
+        return driver.findElement(By.id("logout_sidebar_link"));
+    }
+    public void clickOnTheLogoutFromMenuOption () {
+        getTheLogoutFromMenuOption().click();
+    }
 }
