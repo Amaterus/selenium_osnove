7a2f3e06d15588e619b8168e7f77a846d50cdba1
Veljko Andjelkovic <veljkoandjelkovic2000@gmail.com>
2023-10-12 23:37:34 +0200

    Verify if reset app state is working on the Cart Page

--- src/test/java/tests/SwagLabsTests.java
+++ src/test/java/tests/SwagLabsTests.java
@@ -270,4 +270,12 @@ public class SwagLabsTests extends BasicTest{
         Assert.assertEquals(driver.getCurrentUrl(),
                 baseUrl + "/", "Should be redirected to the login page");
     }
+    @Test (retryAnalyzer = SwagLabsRetry.class)
+    public void verifyIfResetAppStateIsWorking () {
+        verifyIfTheHamburgerButtonIsWorking();
+        inventoryPage.clickOnAddToCartButtonForSauceLabsBackpack();
+        leftNavPage.clickOnTheResetFromMenuOption();
+        topNavPage.getTheShoppingCartButton();
+        Assert.assertTrue(topNavPage.invisibilityOfNumberItemsInCart());
+    }
 }
